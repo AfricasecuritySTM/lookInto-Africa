d2e15a64a4ecc74413c039555e8f468ac6b15b3f
GenZ'_ZeeGah <smartsentinelinfrastructure@gmail.com>
2026-06-12 23:34:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+const button = document.querySelector("button");
 
+button.addEventListener("click", function(){
+
+    alert(
+        "Welcome to lookInto-Africa.\n\nBuilding African Engineering Solutions."
+    );
+
+});
