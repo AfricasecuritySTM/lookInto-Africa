004401bcc1a3fb6b19312a0610a5c15955a32ac9
GenZ'_ZeeGah <smartsentinelinfrastructure@gmail.com>
2026-06-13 00:22:48 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-const button = document.querySelector("button");
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
 
-button.addEventListener("click", function(){
+menuBtn.addEventListener("click", () => {
+
+    if(menu.style.display === "block"){
+
+        menu.style.display = "none";
+
+    }else{
+
+        menu.style.display = "block";
+
+    }
+
+});
+
+const enterBtn = document.getElementById("enterBtn");
+
+enterBtn.addEventListener("click", () => {
 
     alert(
-        "Welcome to lookInto-Africa.\n\nBuilding African Engineering Solutions."
+        "Welcome to lookInto-Africa.\n\nBuild. Document. Collaborate."
     );
 
 });
